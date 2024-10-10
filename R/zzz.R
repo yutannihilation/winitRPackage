6f3dd3581154dd4e751360252021dd67c5cfcf5c
Hiroaki Yutani <yutani.ini@gmail.com>
2024-10-10 18:21:24 +0900

    Do not bundle binary

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,22 @@
+pkg_cache_dir <- function() {
+  normalizePath(tools::R_user_dir("winitRpackage", "cache"), mustWork = FALSE)
+}
+
+server_path <- function() {
+  bin <- if (Sys.info()[["sysname"]] == "Windows") {
+    "winit_r_package_server.exe"
+  } else {
+    "winit_r_package_server"
+  }
+
+  file.path(savvy_cache_dir(), bin)
+
+  stop("TODO")
+}
+
+# overwrite
 class(ExternalWindowController) <- NULL
-`ExternalWindowController`$`new` <- function(server =  system.file("winit_r_package_server.exe", package = "winitRpackage")) {
+`ExternalWindowController`$`new` <- function(server = server_path()) {
   .savvy_wrap_ExternalWindowController(.Call(savvy_ExternalWindowController_new__impl, server))
 }
 class(ExternalWindowController) <- "ExternalWindowController__bundle"
